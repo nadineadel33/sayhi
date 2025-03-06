06890b16fa3870be5f0446d4cfbeeab9338a86cc
20155 <amr90.vip@gmail.com>
2025-03-06 16:04:40 +0200

    Foundation

--- lib/main.dart
+++ lib/main.dart
@@ -19,6 +19,7 @@ class MyApp extends StatelessWidget {
           const Breakpoint(start: 451, end: 800, name: TABLET),
           const Breakpoint(start: 801, end: 1920, name: DESKTOP),
           const Breakpoint(start: 1921, end: double.infinity, name: '4K'),
+          ////////////////////////////////////////////////////////////////
         ],
       ),
       title: 'Sign Language',
